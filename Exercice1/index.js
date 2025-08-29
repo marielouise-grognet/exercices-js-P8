89532051fdcf24e3cc0bf03f0f9962c5c10babf8
Marie-Louise Grognet <marielouise.grognet@gmail.com>
2025-08-29 15:12:34 +0200

    réalisation du premier exercice

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,15 @@
-//votre code ici
+function pairNumbers (min, max) {
+let result = ''
+for (let i = min; i <= max; i++) {
+    if (i % 2 === 0) {
+        if (result !== '') {
+            result += ','
+        }
+        result += i
+    }
+}
+return result 
+}
+console.log(pairNumbers(1,10))
 
 export default pairNumbers
